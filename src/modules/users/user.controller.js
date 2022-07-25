6ab330786a23505ad375b969a7692036bee93d99
Muhammad Rafly El Quraish <elquraishmuhammadrafly@gmail.com>
2022-07-25 20:55:12 +0700

    parsing req into body create user dto

--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -1,4 +1,5 @@
 const UserService = require('./user.service')
+const { UserCreateBody } = require('./user.dto')
 
 module.exports.getUsers = async (req, res) => {
     try {
@@ -21,10 +22,11 @@ module.exports.getUserById = async (req, res) => {
 
 module.exports.createUser = async (req, res) => {
     try {
-        const user = await UserService.createUser(req.body)
+        const userBody = UserCreateBody(req.body)
+        const user = await UserService.createUser(userBody)
         res.success(user)
-    } catch (err) {
-        res.badreq(err?.message)
+    } catch (error) {
+        res.badreq(error?.code ? 'Username has been registred' : error?.errors)
     }
 }
 
